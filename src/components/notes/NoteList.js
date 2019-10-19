04c304a3decb1ad84d818db846453231df827ce5
steffenpedersen <steffen.pedersen@live.dk>
2019-10-19 17:13:35 +0200

    Make NoteSummery component

--- src/components/notes/NoteList.js
+++ src/components/notes/NoteList.js
@@ -1,33 +1,12 @@
 import React from 'react'
+import NoteSummery from './NoteSummery'
 
 const NoteList = () => {
     return (
         <div className="project-list section">
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title ">Note title</span>
-                    <p>Posted by User</p>
-                    <p className="grey-text">19 October, 5pm</p>
-                </div>
-            </div>
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                <span className="card-title ">Note title</span>
-                    <p>Posted by User</p>
-                    <p className="grey-text">19 October, 5pm</p>
-                </div>
-            </div>
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                <span className="card-title ">Note title</span>
-                    <p>Posted by User</p>
-                    <p className="grey-text">19 October, 5pm</p>
-                </div>
-            </div>
-
+            <NoteSummery />
+            <NoteSummery />
+            <NoteSummery />
         </div>
     )
 }
---
+++ src/components/notes/NoteSummery.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const NoteSummery = () => {
+    return (
+        <div className="card z-depth-0 project-summary">
+            <div className="card-content grey-text text-darken-3">
+                <span className="card-title ">Note title</span>
+                <p>Posted by User</p>
+                <p className="grey-text">19 October, 5pm</p>
+            </div>
+        </div>
+    )
+}
+
+export default NoteSummery
