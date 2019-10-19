8c578c1723d85569294462ecde72fa24b411e6a5
steffenpedersen <steffen.pedersen@live.dk>
2019-10-19 17:06:32 +0200

    Add dashboard with dummy data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import NavBar from './components/layout/Navbar'
+import Dashboard from './components/dashboard/Dashboard'
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <NavBar/>
+        <NavBar />
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path='/' component={Dashboard} />
+        </Switch>
       </div>
     </BrowserRouter>
   );
